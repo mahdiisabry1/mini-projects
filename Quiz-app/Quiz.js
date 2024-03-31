bd13104c8d72386acf07f5596d8509a4bcaae3ae
mahdiisabry1 <mahdiisabry@gmail.com>
2024-03-31 11:16:29 +0530

    2

---
+++ Quiz-app/Quiz.js
@@ -0,0 +1,18 @@
+const quizData = [
+    {
+        Question: 'what is the longest river',
+        a : "",
+        b : "",
+        c : "",
+        d : "",
+        correct : "c"
+    },
+    {
+        Question: 'what is the longest river',
+        a : "",
+        b : "",
+        c : "",
+        d : "",
+        correct : "c"
+    }
+]
